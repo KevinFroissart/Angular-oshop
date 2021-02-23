539b61fa4c6aad3940fea60355f22fad1437a711
Froissart Kevin <kevin.froissart69@gmail.com>
2021-02-23 18:30:42 +0100

    bootstrap firebase & components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKAzNqvEoxf7Bibwk6IUr1tOxfRfPTdek",
+    authDomain: "oshop-a34ee.firebaseapp.com",
+    databaseURL: "https://oshop-a34ee-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "oshop-a34ee",
+    storageBucket: "oshop-a34ee.appspot.com",
+    messagingSenderId: "1091938121231",
+    appId: "1:1091938121231:web:408ff58588e18fb54e957a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKAzNqvEoxf7Bibwk6IUr1tOxfRfPTdek",
+    authDomain: "oshop-a34ee.firebaseapp.com",
+    databaseURL: "https://oshop-a34ee-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "oshop-a34ee",
+    storageBucket: "oshop-a34ee.appspot.com",
+    messagingSenderId: "1091938121231",
+    appId: "1:1091938121231:web:408ff58588e18fb54e957a"
+  }
 };
 
 /*
